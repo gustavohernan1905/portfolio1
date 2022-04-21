95085f02c8b168ddf1b85664004a7429437de913
gustavohernan1905 <guhediaz@gmail.com>
2022-04-21 16:41:42 -0300

    cambios

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                    <a class="nav-link px-4 py-1 px-sm-4<?php echo $pg == "index"? "active":"" ?>" href="index.php">Inicio</a>
+                    <a class="nav-link py-1 px-sm-4 <?php echo $pg == "index"? "active":"" ?>" href="index.php">Inicio</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link py-1 px-3 px-sm-4 <?php echo $pg == "sobre-mi"? "active":"" ?>" href="sobre-mi.php">Sobre mí</a>
