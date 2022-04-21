e79e1249c25430865b720a21118896f145c9160d
gustavohernan1905 <guhediaz@gmail.com>
2022-04-21 16:57:12 -0300

    cambios

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer class="container">
-    <div class="row">
-        <div class="col-12 col-sm-3">
+    <div class="row mt-5 pb-3">
+        <div class="col-12 col-sm-3 text-center text-sm-start">
             <a href="https://github.com/"><i class="fa-brands fa-github"></i></a>
             <a href="https://www.linkedin.com/in/gustavo-hern%C3%A1n-d%C3%ADaz-b190671bb/"><i class="fa-brands fa-linkedin"></i></a>
         </div>
-        <div class="col-12 col-sm-3"> Sponsor <a href="https://dpcsuite.com">DePc Suite</a></div>
-        <div class="col-12 col-sm-3"><a href="mailto:guhediaz@gmail.com">guhediaz@gmail.com</a></div>
+        <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0"> Sponsor <a href="https://dpcsuite.com">DePc Suite</a></div>
+        <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0"><a href="mailto:guhediaz@gmail.com">guhediaz@gmail.com</a></div>
     </div>
 </footer>
